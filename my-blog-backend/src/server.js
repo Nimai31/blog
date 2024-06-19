265003f1c8b3641601075b5b20d51208deab72c5
Nimai31 <nimaipoleplli@gmail.com>
2024-06-19 20:52:13 +0530

    commit

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -10,13 +10,6 @@ admin.initializeApp({
 });
 
 const app = express();
-app.use(
-  cors({
-    origin: ["https://deploy-mern-1whq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
 app.use(express.json());
 
 app.use(async (req, res, next) => {
