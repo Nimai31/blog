7c1d9723d11246589380aee84a1dd203c8084bb0
Nimai31 <nimaipoleplli@gmail.com>
2024-06-19 12:51:32 +0530

    commit before delete

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,52 +1,105 @@
+import fs from "fs";
+import admin from "firebase-admin";
 import express from "express";
+import { db, connectToDb } from "./db.js";
 
-let articlesInfo = [
-  {
-    name: "learn-react",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "learn-node",
-    upvotes: 0,
-    comments: [],
-  },
-  {
-    name: "mongodb",
-    upvotes: 0,
-    comments: [],
-  },
-];
+const credentials = JSON.parse(fs.readFileSync("./credentials.json"));
+admin.initializeApp({
+  credential: admin.credential.cert(credentials),
+});
 
 const app = express();
 app.use(express.json());
 
-app.put("/api/articles/:name/upvote", (req, res) => {
+app.use(async (req, res, next) => {
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authtoken);
+    } catch (e) {
+      return res.sendStatus(400);
+    }
+  }
+
+  req.user = req.user || {};
+
+  next();
+});
+
+app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-  const article = articlesInfo.find((a) => a.name === name);
+  const { uid } = req.user;
+
+  const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    article.upvotes += 1;
-    res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`);
+    const upvoteIds = article.upvoteIds || [];
+    article.canUpvote = uid && !upvoteIds.includes(uid);
+    res.json(article);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.use((req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+app.put("/api/articles/:name/upvote", async (req, res) => {
+  const { name } = req.params;
+  const { uid } = req.user;
+
+  const article = await db.collection("articles").findOne({ name });
+
+  if (article) {
+    const upvoteIds = article.upvoteIds || [];
+    const canUpvote = uid && !upvoteIds.includes(uid);
+
+    if (canUpvote) {
+      await db.collection("articles").updateOne(
+        { name },
+        {
+          $inc: { upvotes: 1 },
+          $push: { upvoteIds: uid },
+        }
+      );
+    }
+
+    const updatedArticle = await db.collection("articles").findOne({ name });
+    res.json(updatedArticle);
   } else {
     res.send("That article doesn't exist");
   }
 });
 
-app.post("/api/articles/:name/comments", (req, res) => {
+app.post("/api/articles/:name/comments", async (req, res) => {
   const { name } = req.params;
-  const { postedBy, text } = req.body;
+  const { text } = req.body;
+  const { email } = req.user;
 
-  const article = articlesInfo.find((a) => a.name === name);
+  await db.collection("articles").updateOne(
+    { name },
+    {
+      $push: { comments: { postedBy: email, text } },
+    }
+  );
+  const article = await db.collection("articles").findOne({ name });
 
   if (article) {
-    article.comments.push({ postedBy, text });
-    res.send(article.comments);
+    res.json(article);
   } else {
     res.send("That article doesn't exist!");
   }
 });
 
-app.listen(8000, () => {
-  console.log("Server is listening on port 8000");
+connectToDb(() => {
+  console.log("Successfully connected to database!");
+  app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
+  });
 });
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAe6ynvcE3MZbu7OmTM5YcsNEt8W52Em90",
+  authDomain: "my-react-blog-a9c2f.firebaseapp.com",
+  projectId: "my-react-blog-a9c2f",
+  storageBucket: "my-react-blog-a9c2f.appspot.com",
+  messagingSenderId: "241101641467",
+  appId: "1:241101641467:web:e7801a15102d6d5554825e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
